96c6f21c45c0168eba3646b9659bdc9bbc30e4a5
amysthat <arduinomacerasi@gmail.com>
2025-02-04 23:55:13 +0300

    first mvp

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const fetchFile = (file, onfullfill) => {
+    fetch(file)
+        .then((res) => res.text())
+        .then(onfullfill)
+        .catch((e) => console.error(e));
+}
+
+const setRandomQuote = () => {
+    const line = lines[Math.floor(Math.random() * lines.length)];
+
+    document.getElementById("quote").innerText = line;
+}
+
+const turkish_specific_available = Intl.DateTimeFormat().resolvedOptions().timeZone.includes("Istanbul");
+
+const loadTurkish = () => {
+    fetchFile("messages/tr.txt", (text) => {
+        lines = lines.concat(text.split('\n').filter(elm => elm));
+
+        if (turkish_specific_available) {
+            fetchFile("messages/tr_turk.txt", (text) => {
+                lines = lines.concat(text.split('\n').filter(elm => elm));
+
+                setRandomQuote();
+            });
+        }
+        else {
+            setRandomQuote();
+        }
+    });
+}
+
+const loadEnglish = () => {
+    fetchFile("messages/en.txt", (text) => {
+        lines = lines.concat(text.split('\n').filter(elm => elm));
+
+        setRandomQuote();
+    });
+}
+
+let lines = [];
+
+if (turkish_specific_available || navigator.language.includes("tr")) {
+    loadTurkish();
+}
+else {
+    loadEnglish();
+}
